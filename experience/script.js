580dc4e3abdeca1bc353a1595eb5318bbdb56ab0
shreyasparaj <21BCS6332@cuchd.in>
2024-02-24 01:17:04 +0530

    Changed scripts and css

--- experience/script.js
+++ experience/script.js
@@ -31,25 +31,6 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 
 
-// disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
-
 document.addEventListener('visibilitychange',
 function(){
     if(document.visibilityState === "visible"){
