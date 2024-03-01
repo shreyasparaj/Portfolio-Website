ee54cbd4e88b2c86b17d1485b6b7d85eb984b088
shreyasparaj <21BCS6332@cuchd.in>
2024-03-01 16:13:51 +0530

    Fixed script,js

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,6 +181,7 @@ srtop.reveal('.home .linkedin', { interval: 600 });
 srtop.reveal('.home .github', { interval: 800 });
 srtop.reveal('.home .twitter', { interval: 1000 });
 srtop.reveal('.home .telegram', { interval: 600 });
+srtop.reveal('.home .kaggle', { interval: 600 });
 srtop.reveal('.home .instagram', { interval: 600 });
 srtop.reveal('.home .dev', { interval: 600 });
 
